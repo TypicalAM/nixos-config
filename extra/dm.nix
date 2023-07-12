06314223d7b293e1467c82207ba6647dfac18014
TypicalAM <tpam_piaseczny@protonmail.com>
2023-07-13 01:45:51 +0200

    add the kvantum themes and start on the qt5ct config

--- extra/dm.nix
+++ extra/dm.nix
@@ -1,35 +1,40 @@
 { config, pkgs, ...}:
 
-{
+let
+	sddm-chili = pkgs.fetchFromGitHub {
+    owner = "MarianArlt";
+    repo = "sddm-chili";
+    rev = "6516d50176c3b34df29003726ef9708813d06271";
+    sha256 = "wxWsdRGC59YzDcSopDRzxg8TfjjmA3LHrdWjepTuzgw=";
+	};
+in {
   # Window manager stuff
 	environment.systemPackages = with pkgs; [ 
 		libsForQt5.qt5.qtgraphicaleffects
 		libsForQt5.qtstyleplugin-kvantum
-		catppuccin-kvantum
 		lxappearance
 		qt5ct
 	];
 
+	# Set the platformTheme here because it's buggy in HM
 	qt = {
 		enable = true;
 		platformTheme = "qt5ct";
 	};
 
-  services.xserver = {
+	services.xserver = {
     enable = true;
     layout = "pl";
     xkbVariant = "";
 
 		desktopManager.xterm.enable = false;
 		displayManager = {
-			sddm.enable = true;
-			sddm.theme = "${(pkgs.fetchFromGitHub {
-    		owner = "MarianArlt";
-    		repo = "sddm-chili";
-    		rev = "6516d50176c3b34df29003726ef9708813d06271";
-    		sha256 = "wxWsdRGC59YzDcSopDRzxg8TfjjmA3LHrdWjepTuzgw=";
-			})}";
 			defaultSession = "none+i3";
+
+			sddm = {
+				enable = true;
+				theme = "${sddm-chili}";
+			};
 		};
 
 		windowManager.i3 = {
--- home/programs/theming.nix
+++ home/programs/theming.nix
@@ -1,6 +1,41 @@
 { config, pkgs, ... }:
 
-{
+let
+	catppuccin-theme = pkgs.fetchFromGitHub {
+			owner = "catppuccin";
+			repo = "Kvantum";
+			rev = "04be2ad3d28156cfb62478256f33b58ee27884e9";
+			sha256 = "apOPiVwePXbdKM1/0HAfHzIqAZxvfgL5KHzhoIMXLqI=";
+	};
+in {
+	home.packages = with pkgs; [
+		tela-icon-theme
+	];
+
+	xdg = {
+		configFile = {
+			"Kvantum/Catpuccin-Mocha-Lavender/Catpuccin-Mocha-Lavender.kvconfig".source = "${catppuccin-theme}/src/Catppuccin-Mocha-Lavender/Catppuccin-Mocha-Lavender.kvconfig";
+			"Kvantum/Catpuccin-Mocha-Lavender/Catpuccin-Mocha-Lavender.svg".source = "${catppuccin-theme}/src/Catppuccin-Mocha-Lavender/Catppuccin-Mocha-Lavender.svg";
+      "Kvantum/kvantum.kvconfig".text = ''
+				[General]
+				theme=Catpuccin-Mocha-Lavender
+			'';
+			"qt5ct/qt5ct.conf".text = ''
+				[Appearance]
+				color_scheme_path=${pkgs.qt5ct}/colors/airy.conf
+				custom_palette=false
+				icon_theme=McMojave-circle-purple
+				standard_dialogs=default
+				style=kvantum
+			'';
+		};
+	};
+
+	gtk.iconTheme = {
+		name = "Tela-Icon-Theme";
+		package = pkgs.tela-icon-theme;
+	};
+
 	services.picom = {
 		enable = true;
 		fade = true;
