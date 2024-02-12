e6262ef124ae13c3ba05576b74b82b600fc465ed
TypicalAM <tpam_piaseczny@protonmail.com>
2024-02-13 00:53:15 +0100

    Misc operational

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,8 @@ in {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    git-crypt
+    libnotify
     kolourpaint
     kdenlive
     youtube-dl
